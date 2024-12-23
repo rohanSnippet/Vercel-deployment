0e3eb0f786410af830edcca15a36143c458cfa03
rohanSnippet <rohan110620@gmail.com>
2024-12-23 12:08:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/hello', (req, res) => {
+    res.send('This is Hello ')
+  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
